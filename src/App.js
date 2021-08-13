debc56f341da27057a94ca39a13703e9ddc68c7f
jeffreytram <jeffrey_tram@yahoo.com>
2021-08-13 16:09:16 -0400

    Created day button componenet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import { faArrowLeft, faArrowRight, faSun, faMoon } from '@fortawesome/free-soli
 import { renderChart, drawCanvasBars } from './logic/chart.js';
 import './App.css';
 
+const DayButton = (props) => {
+  const { abbrevation, fullName, displayName } = props;
+  return (
+    <label htmlFor={abbrevation}>
+      <input type="checkbox" name={abbrevation} id={abbrevation} value={fullName} />
+      <span className="checkbox">{displayName}</span>
+    </label>
+  )
+}
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -27,7 +36,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <>
         <div id="loading">
           <div className="lds-dual-ring"></div>
           <h2>Loading...</h2>
@@ -38,10 +47,10 @@ class App extends React.Component {
               <div id="theme-switcher-container">
                 <div id="theme-switcher" onClick={this.toggleDarkTheme}>
                   {
-                    (this.state.isDarkTheme) ? 
-                    (<FontAwesomeIcon icon={faSun}/>)
-                    :
-                    (<FontAwesomeIcon icon={faMoon}/>)
+                    (this.state.isDarkTheme) ?
+                      (<FontAwesomeIcon icon={faSun} />)
+                      :
+                      (<FontAwesomeIcon icon={faMoon} />)
                   }
                 </div>
               </div>
@@ -62,34 +71,13 @@ class App extends React.Component {
                 <div id="day-filters">
                   <label>Filter by day of the week:</label>
                   <div id="day-container">
-                    <label htmlFor="mon">
-                      <input type="checkbox" name="mon" id="mon" value="Monday" />
-                      <span className="checkbox">Mon</span>
-                    </label>
-                    <label htmlFor="tues">
-                      <input type="checkbox" name="tues" id="tues" value="Tuesday" />
-                      <span className="checkbox">Tue</span>
-                    </label>
-                    <label htmlFor="wed">
-                      <input type="checkbox" name="wed" id="wed" value="Wednesday" />
-                      <span className="checkbox">Wed</span>
-                    </label>
-                    <label htmlFor="thurs">
-                      <input type="checkbox" name="thurs" id="thurs" value="Thursday" />
-                      <span className="checkbox">Thu</span>
-                    </label>
-                    <label htmlFor="fri">
-                      <input type="checkbox" name="fri" id="fri" value="Friday" />
-                      <span className="checkbox">Fri</span>
-                    </label>
-                    <label htmlFor="sat">
-                      <input type="checkbox" name="sat" id="sat" value="Saturday" />
-                      <span className="checkbox">Sat</span>
-                    </label>
-                    <label htmlFor="sun">
-                      <input type="checkbox" name="sun" id="sun" value="Sunday" />
-                      <span className="checkbox">Sun</span>
-                    </label>
+                    <DayButton abbrevation="mon" fullName="Monday" displayName="Mon" />
+                    <DayButton abbrevation="tues" fullName="Tuesday" displayName="Tue" />
+                    <DayButton abbrevation="wed" fullName="Wednesday" displayName="Wed" />
+                    <DayButton abbrevation="thurs" fullName="Thursday" displayName="Thu" />
+                    <DayButton abbrevation="fri" fullName="Friday" displayName="Fri" />
+                    <DayButton abbrevation="sat" fullName="Saturday" displayName="Sat" />
+                    <DayButton abbrevation="sun" fullName="Sunday" displayName="Sun" />
                   </div>
                 </div>
                 <button id="reset" className="button">Reset</button>
@@ -123,10 +111,10 @@ class App extends React.Component {
                 </span>
                 <div id="date-navigation">
                   <button id="left" className="arrow">
-                    <FontAwesomeIcon icon={faArrowLeft}/>
+                    <FontAwesomeIcon icon={faArrowLeft} />
                   </button>
                   <button id="right" className="arrow">
-                    <FontAwesomeIcon icon={faArrowRight}/>
+                    <FontAwesomeIcon icon={faArrowRight} />
                   </button>
                   <select id="date-range">
                   </select>
@@ -140,7 +128,7 @@ class App extends React.Component {
         <datalist id="artist-datalist"></datalist>
         <datalist id="song-datalist"></datalist>
         <datalist id="album-datalist"></datalist>
-      </div>
+      </>
     );
   }
 }
