253e73849164bd047fe2174e2ec2142921fea116
jeffreytram <jeffrey_tram@yahoo.com>
2022-01-01 19:15:04 -0500

    Changed class name to be more concise

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,7 +6,7 @@ import { faRedoAlt } from '@fortawesome/free-solid-svg-icons'
 const PointSetting = ({ title, settingName, dispatchPointSettings, pointSettings }) => {
   return (
     <div className="point-setting">
-      <div className="settings-flexbox">
+      <div className="setting-flexbox">
         <>
           <label htmlFor={`point-${settingName}-setting`}>{title}</label>
           <input type="range" step={.01} min={.5} max={1.5} value={pointSettings['constant'][settingName]} onChange={(event) => dispatchPointSettings({ setting: settingName, value: event.target.value })} id={`point-${settingName}-setting`} />
@@ -18,8 +18,8 @@ const PointSetting = ({ title, settingName, dispatchPointSettings, pointSettings
 
 function Settings({ dispatchPointSettings, pointSettings }) {
   return (
-    <div className="point-settings-container">
-      <div className="settings-container">
+    <div className="settings-content">
+      <div className="settings-input-container">
         <PointSetting
           title='Point Opacity'
           settingName='opacity'
